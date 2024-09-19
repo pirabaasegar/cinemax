c07f9e777d532090b6c84b8b64322f8d20627701
pirabaasegar <61627407+pirabaasegar@users.noreply.github.com>
2024-09-19 11:30:26 -0400

    ---

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import './index.css';
+import './index.css';  
 import Trending from './Trending';
 import Movies from './Movies';
 import TV from './TV';
 import Search from './Search';
+import MovieDetail from './MovieDetail';  // Import your movie detail component
 
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';  
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const container = document.getElementById('root');
@@ -16,11 +17,12 @@ const root = createRoot(container);
 
 root.render(
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Trending />} />
-    <Route path="/movies" element={<Movies />} />
-    <Route path="/tv" element={<TV />} />
-    <Route path="/search" element={<Search />} />
-  </Routes>
-</BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Trending />} />             {/* Home Route */}
+      <Route path="/movies" element={<Movies />} />         {/* Movies Route */}
+      <Route path="/tv" element={<TV />} />                {/* TV Shows Route */}
+      <Route path="/search" element={<Search />} />         {/* Search Route */}
+      <Route path="/movie/:movieSlug" element={<MovieDetail />} />  {/* Dynamic Movie Route */}
+    </Routes>
+  </BrowserRouter>
 );
